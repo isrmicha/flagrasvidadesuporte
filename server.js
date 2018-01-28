2c1724539047a71dda54871f8586366faa74caa1
Israel <isrmicha@gmail.com>
2018-01-28 10:16:03 -0200

    e

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-const express = require('express')
+const express = require('express');
 const app = express()
-var path    = require("path");
+var request = require('request');
+setInterval(()=>{
+	request('http://flagrasvidadesuporte.herokuapp.com/', function (error, response, body) {
+  console.log('Pingando em ' + new Date());
+});
+},15 * 60 * 1000);
+
 app.use(express.static('public'));
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port '+process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port  '+ 3000))
